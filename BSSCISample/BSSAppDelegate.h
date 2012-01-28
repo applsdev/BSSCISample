1efbde5efc9dde81e295020006560bc811bc43fc
Andre Guggenberger <guan@gmx.at>
2012-01-28 14:07:21 +0100

    another update

--- BSSCISample/BSSAppDelegate.h
+++ BSSCISample/BSSAppDelegate.h
@@ -3,7 +3,7 @@
 //  BSSCISample
 //
 //  Created by Martin Reichart on 12/25/11.
-//  Copyright (c) 2011 Blacksmith Software. All rights reserved.
+//  Copyright (c) 2011 Blacksmith Software. All rights reserved.   
 //
 
 #import <UIKit/UIKit.h>
